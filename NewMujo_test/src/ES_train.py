f7b8fe0a5a2f4e5ee4497d826af76915fe8e9fb2
GrumpySloths <1273886989@qq.com>
2023-10-23 08:52:16 +0800

    test

--- NewMujo_test/src/ES_train.py
+++ NewMujo_test/src/ES_train.py
@@ -53,6 +53,7 @@ ES_EVERY_STEPS = 5e4
 ETG_TRAIN_STPES=30
 ETG_TRAIN_STPES_LOWBOUND=10 #ETG训练步骤的下限
 #______________________________
+print("hello world")
 def debug(info):
     if DEBUG:
         print(info)
